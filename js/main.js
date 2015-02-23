e325f840850f78dd473669b2d2a8f4816943245b
dannytranlx <danny.tran@radialpoint.com>
2015-02-23 12:57:33 -0500

    Update forms layout and javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-$('#formCours').submit(function() {
-  var lien = "http://etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/Fiche-de-cours?Sigle=" + $('#sigleCours').val();
+$('#formCours').submit(function(e) {
+  e.preventDefault();
+  var lien = 'http://etsmtl.ca/Etudiants-actuels/Baccalaureat/Cours-horaires-1er-cycle/Fiche-de-cours?Sigle=' + $('#sigleCours').val();
   window.location = lien;
-  return false;
 });
 
-$('#btnBandwidth3').click(function() {
-	window.location = "http://bw.etsmtl.me/"+$('#phase').val()+"/"+$('#app').val();
+$('#formBandwidth').submit(function(e) {
+  e.preventDefault();
+  window.location = 'http://bw.etsmtl.me/' + $('#phase').val() + '/' + $('#app').val();
 });
